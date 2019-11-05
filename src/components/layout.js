dc7b26d40c7f595e5b6e317c5c8f3ed4554890c7
zhan ishzhanov <jey.and.key@gmail.com>
2019-11-05 09:17:34 +0000

    styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import 'semantic-ui-css/semantic.min.css'
 import "./layout.css"
 // import "../main.scss"
 // import "uikit/dist/css/uikit.min.css"
